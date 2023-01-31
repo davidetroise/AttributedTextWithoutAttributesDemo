4556385c2453e6484290c40592f9ffb4a8e9faf3
Davide Troise <davide@troise.net>
2023-01-31 23:28:55 +0100

    Initial Commit

---
+++ AttributedTextWithoutAttributesDemo/AttributedTextWithoutAttributesDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  AttributedTextWithoutAttributesDemoApp.swift
+//  AttributedTextWithoutAttributesDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct AttributedTextWithoutAttributesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
